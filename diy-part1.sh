b6202c907208a412d308bb44142f1c7f588da8eb
MyTol <nishiwov1.xiaofang@gmail.com>
2024-04-29 21:09:51 +0800

    Update diy-part1.sh
    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,18 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 添加 5G 支持
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/Modem-Support
+
+# 添加风扇控制
+git clone --depth=1 https://github.com/2253845067/h69k-fanctrl package/h69k-fanctrl
+
+# 添加 MT7916 160Mhz 修复
+rm -rf package/kernel/mt76
+git clone --depth=1 https://github.com/2253845067/mt76 package/kernel/mt76
