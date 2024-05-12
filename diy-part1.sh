eb9378cbcc9933e6188651a783451c438a7175db
MyTol <nishiwov1.xiaofang@gmail.com>
2024-05-13 01:00:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,13 +17,10 @@ sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.15/g" ./target
 # Small 大佬常用 OpenWrt 软件包 冲突处理
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb,filebrowser,luci-app-filebrowser}
-# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb,sms-tool,luci-app-sms-tool,filebrowser,luci-app-filebrowser}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb,sms-tool,luci-app-sms-tool,filebrowser,luci-app-filebrowser}
 
 # 添加 5G 支持
 # git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/modem
-git clone --depth=1 https://github.com/MyTol/sendat package/sendat
-git clone --depth=1 https://github.com/MyTol/luci-app-cpe package/luci-app-cpe
 
 # 降低风扇噪音
 git clone --depth=1 https://github.com/MyTol/h69k-fanctrl package/h69k-fanctrl
@@ -34,3 +31,6 @@ git clone --depth=1 https://github.com/2253845067/mt76 package/kernel/mt76
 
 # 编译 luci-app-filebrowser 应用
 git clone --depth=1 https://github.com/wangqn/luci-app-filebrowser package/luci-app-filebrowser
+
+# 编译 luci-app-dockerman 应用
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
