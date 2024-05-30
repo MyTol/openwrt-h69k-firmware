ddd25a170fa9dc0d04b7d05df6c89e2222d1a7f9
MyTol <nishiwov1.xiaofang@gmail.com>
2024-05-31 01:59:48 +0800

    Update diy-part2.sh

--- config/lede/diy-part2.sh
+++ config/lede/diy-part2.sh
@@ -8,9 +8,9 @@
 ## 设置 IPV4 地址
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 ## 设置 IPV6 分发长度
-sed -i 's/ip6assign=\'60\'/ip6assign=\'64\'/g' package/base-files/files/bin/config_generate
+sed -i "s/ip6assign='60'/ip6assign='64'/g" package/base-files/files/bin/config_generate
 ## 删除 IPV6 ULA 头
-sed -i 's/ula_prefix=\'auto\'/ula_prefix=\'\'/' package/base-files/files/bin/config_generate
+sed -i "s/ula_prefix='auto'/ula_prefix=''/" package/base-files/files/bin/config_generate
 ## 设置主机名称
 sed -i 's/OpenWrt/OmO/g' package/base-files/files/bin/config_generate
 ## 删除 Dropbear 接口
