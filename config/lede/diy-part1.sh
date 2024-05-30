33538b73e48de8307a2322fda08db4cb3a308699
MyTol <nishiwov1.xiaofang@gmail.com>
2024-05-31 02:21:55 +0800

    Update diy-part1.sh

--- config/lede/diy-part1.sh
+++ config/lede/diy-part1.sh
@@ -9,12 +9,16 @@ rm -rf ./package/lean/autocore
 sed -i "1isrc-git extraipk https://github.com/MyTol/extra-ipk\n" feeds.conf.default
 sed -i "2isrc-git wwan https://github.com/MyTol/5G-Modem-Support\n" feeds.conf.default
 
-# 调整内核版本 5.15
+## 调整内核版本 5.15
 sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.15/g" ./target/linux/rockchip/Makefile
 sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.15/g" ./target/linux/rockchip/Makefile
 
 ## 更新软件源
 ./scripts/feeds update -a
+
+## 移除冲突包
+rm -rf package/feeds/wwan/bwallocate
+rm -rf package/feeds/extraipk/luci-app-wechatpush
 
 # 拉取风扇降噪补丁
 git clone --depth=1 https://github.com/MyTol/h69k-fanctrl package/h69k-fanctrl
