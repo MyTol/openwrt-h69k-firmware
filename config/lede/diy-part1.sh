898bc6f1d147e63b2e97caab2d7c7fc06f95176b
MyTol <nishiwov1.xiaofang@gmail.com>
2024-06-01 00:46:35 +0800

    Update diy-part1.sh

--- config/lede/diy-part1.sh
+++ config/lede/diy-part1.sh
@@ -13,29 +13,25 @@ sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.15/g" ./target
 ## 更新软件源
 ./scripts/feeds update -a
 
-## 移除冲突包
+## 拉取补丁
 rm -rf package/wwan
 rm -rf package/kernel/mt76
-rm -rf package/lean/autocore
-rm -rf package/feeds/extraipk/{luci-app-filebrowser,luci-app-dockerman,luci-app-wechatpush}
-
-## 拉取 5G 支持
+## 拉取 5G 补丁
 git clone --depth=1 https://github.com/MyTol/5G-Modem-Support package/wwan
-
-## 拉取风扇降噪插件
-git clone --depth=1 https://github.com/MyTol/h69k-fanctrl package/h69k-fanctrl
-#
-## 移动风扇强冷补丁
+## 风扇降噪补丁
+git clone --depth=1 https://github.com/MyTol/h69k-fanctrl package/fanctrl
+## 风扇强冷补丁
 # cp -af feeds/extraipk/patch/rockchip/* target/linux/rockchip/armv8/base-files/
-
-# 拉取 MT7916 160Mhz 修复补丁
+## 拉取 MT7916 160Mhz 补丁
 git clone --depth=1 https://github.com/2253845067/mt76 package/kernel/mt76
-
-# 拉取 luci-app-filebrowser 应用
-git clone --depth=1 https://github.com/wangqn/luci-app-filebrowser package/luci-app-filebrowser
-
-## 移除无用包
-rm -rf package/wwan/rooter/0optionalapps/bwallocate
 
 ## 安装软件源
 ./scripts/feeds install -a
+
+## 移除冲突包
+rm -rf package/lean/autocore
+rm -rf package/wwan/rooter/0optionalapps/bwallocate
+rm -rf package/feeds/extraipk/{luci-app-filebrowser,luci-app-dockerman,luci-app-wechatpush}
+
+# 拉取 luci-app-filebrowser 应用
+git clone --depth=1 https://github.com/wangqn/luci-app-filebrowser package/feeds/luci/luci-app-filebrowser
