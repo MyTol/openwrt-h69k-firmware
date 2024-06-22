e476775704861b1c9ae44cf1dee5a1333f9bee58
MyTol <nishiwov1.xiaofang@gmail.com>
2024-06-22 21:26:45 +0800

    修改编译配置

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -13,16 +13,14 @@
 # sed -i 's/6.1/5.15/g' target/linux/rockchip/Makefile
 
 # 添加 luci-app-modem
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/modem
-cp -r package/modem/luci-app-modem package/wwan/app
-rm -rf package/modem
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/wwan/modem
 
 # 添加 luci-app-oled
-git clone --depth=1 https://github.com/NateLol/luci-app-oled feeds/luci/applications/luci-app-oled
+#git clone --depth=1 https://github.com/NateLol/luci-app-oled feeds/luci/applications/luci-app-oled
 
 # 添加风扇补丁
 git clone --depth=1 https://github.com/2253845067/h69k-fanctrl package/h69k-fanctrl
 
 # MT7916 160mhz 修复
-rm -rf package/kernel/mt76
-git clone --depth=1 https://github.com/2253845067/mt76 package/kernel/mt76
+#rm -rf package/kernel/mt76
+#git clone --depth=1 https://github.com/2253845067/mt76 package/kernel/mt76
