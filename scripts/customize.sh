ed1982438dd0544d67830664adb0c2ffacdfcae9
MyTol <nishiwov1.xiaofang@gmail.com>
2024-06-22 21:20:28 +0800

    修改编译配置

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -17,4 +17,4 @@
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名称
-sed -i 's/OpenWrt/OmO/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OmO/g' package/base-files/files/bin/config_generate
