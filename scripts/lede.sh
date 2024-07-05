02330b9bf8d66576e87d3f909f12a9fb8a6d6d7f
MyTol <nishiwov1.xiaofang@gmail.com>
2024-07-05 08:08:33 +0800

    修改编译脚本

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -13,85 +13,14 @@
 mkdir package/community
 pushd package/community
 
-# 添加 xiangfeidexiaohuo 存储库
-git clone --depth=1 https://github.com/xiangfeidexiaohuo/extra-ipk
-cp -r ./extra-ipk/op-ddnsgo ./
-cp -r ./extra-ipk/luci-app-iperf3-server ./
-rm -rf extra-ipk
-
-# 添加 Lienol 存储库
-git clone --depth=1 https://github.com/Lienol/openwrt-package
-rm -rf ../../feeds/luci/applications/luci-app-kodexplorer
-rm -rf openwrt-package/verysync
-rm -rf openwrt-package/luci-app-filebrowser
-rm -rf openwrt-package/luci-app-verysync
-
-# 添加 luci-app-filebrowser
-#git clone --depth=1 https://github.com/wangqn/luci-app-filebrowser luci-app-filebrowser
-
 # 添加 h69k-fanctrl
 git clone --depth=1 https://github.com/2253845067/h69k-fanctrl h69k-fanctrl
 
-# 添加 luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-
-# 添加 luci-app-oled
-rm -rf ../../feeds/luci/applications/luci-app-oled
-git clone --depth=1 https://github.com/NateLol/luci-app-oled
-
-# 添加 luci-app-poweroff
-# git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
-
-# 添加 luci-theme
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config luci-app-argon-config
-rm -rf ../../feeds/luci/themes/luci-theme-argon
-rm -rf ../../feeds/luci/themes/luci-theme-argon-mod
-rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/data/background.jpg luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-git clone --depth=1 https://github.com/DHDAXCW/theme
 popd
 
-# MT7916 160mhz 修复
-#rm -rf package/kernel/mt76
-#git clone --depth=1 https://github.com/2253845067/mt76 package/kernel/mt76
-
-# 添加上游 5G 支持
+# 添加 5G 支持
 rm -rf package/wwan
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/wwan
-# 修改接口名
-sed -i 's/wwan_5g_${modem_no}/wwan_${modem_no}/g' package/wwan/luci-app-modem/root/etc/init.d/modem
-sed -i 's/wwan6_5g_${modem_no}/wwan6_${modem_no}/g' package/wwan/luci-app-modem/root/etc/init.d/modem
-sed -i 's/wwan_5g_${modem_no}/wwan_${modem_no}/g' package/wwan/luci-app-modem/root/usr/share/modem/modem_network_task.sh
-sed -i 's/wwan6_5g_${modem_no}/wwan6_${modem_no}/g' package/wwan/luci-app-modem/root/usr/share/modem/modem_network_task.sh
-# 修复 FM160-CN 电话号码获取
-rm -rf package/wwan/luci-app-modem/root/usr/share/modem/fibocom.sh
-cp -f $GITHUB_WORKSPACE/data/modem/fibocom.sh package/wwan/luci-app-modem/root/usr/share/modem/fibocom.sh
-# 修改页面布局
-rm -rf package/wwan/luci-app-modem/luasrc/view/modem/modem_info.htm
-cp -f $GITHUB_WORKSPACE/data/view/modem_info.htm package/wwan/luci-app-modem/luasrc/view/modem/modem_info.htm
-rm -rf package/wwan/luci-app-modem/luasrc/controller/modem.lua
-cp -f $GITHUB_WORKSPACE/data/modem/modem.lua package/wwan/luci-app-modem/luasrc/controller/modem.lua
-
-# 调整 luci-app-passwall 菜单入口
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/passwall/*.lua
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/model/cbi/passwall/client/*.lua
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/model/cbi/passwall/server/*.lua
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/app_update/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/socks_auto_switch/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/global/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/haproxy/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/log/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/node_list/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/rule/*.htm
-sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/view/passwall/server/*.htm
-
-# 调整 luci-app-fileassistant 菜单入口
-#sed -i 's/nas/system/g' package/community/openwrt-package/luci-app-fileassistant/luasrc/controller/*.lua
-#sed -i 's/, 1)/, 89)/g' package/community/openwrt-package/luci-app-fileassistant/luasrc/controller/*.lua
-#sed -i 's/nas/system/g' package/community/openwrt-package/luci-app-fileassistant/htdocs/luci-static/resources/fileassistant/*.js
+git clone --depth=1 https://github.com/my-world-only-me/modem package/wwan
 
 # 修改 zzz-default-settings
 pushd package/lean/default-settings/files
--- scripts/preset-other.sh
+++ scripts/preset-other.sh
@@ -10,21 +10,6 @@
 # See /LICENSE for more information.
 #
 
-# 插件设置
-#
-## luci-app-oled
-pushd package/community/luci-app-oled/luci-app-oled
-sed -i "s|enable '0'|enable '1'|g" root/etc/config/oled
-sed -i "s|netspeed '0'|netspeed '1'|g" root/etc/config/oled
-sed -i "s|time '60'|time '300'|g" root/etc/config/oled
-sed -i "s|text 'OPENWRT'|text 'OmO~~'|g" root/etc/config/oled
-sed -i "s|netsource 'eth0'|netsource 'wwan0'|g" root/etc/config/oled
-popd
-## luci-app-gowebdav
-sed -i 's|6086|8083|g' feeds/packages/net/gowebdav/files/gowebdav.config
-sed -i 's|user|OmO|g' feeds/packages/net/gowebdav/files/gowebdav.config
-sed -i 's|pass|password|g' feeds/packages/net/gowebdav/files/gowebdav.config
-sed -i 's|/mnt|/home|g' feeds/packages/net/gowebdav/files/gowebdav.config
 
 # 修改默认地址
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
