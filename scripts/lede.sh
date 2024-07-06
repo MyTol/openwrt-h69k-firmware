a2fd57dbf5fe486090a8c61e519dbae9ffdd0c92
MyTol <nishiwov1.xiaofang@gmail.com>
2024-07-06 15:55:15 +0800

    修改编译脚本

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -11,7 +11,7 @@
 # 插件更改
 #
 ## 调整 luci-app-modem
-#source scripts/preset-modem.sh
+source scripts/preset-modem.sh
 ## 调整 luci-app-passwall 菜单
 sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/controller/*.lua
 sed -i 's/services/vpn/g' package/community/openwrt-passwall/luci-app-passwall/luasrc/passwall/*.lua
