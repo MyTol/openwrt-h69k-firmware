fc5dd916ce0dcbae064a072e5655666081ce7104
MyTol <nishiwov1.xiaofang@gmail.com>
2024-07-06 09:04:27 +0800

    修改编译脚本

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -9,6 +9,7 @@
 #=================================================
 
 # 修复 AutoCore 样式
+cp -f $GITHUB_WORKSPACE/data/autocore/cpuinfo package/lean/autocore/files/arm/sbin/cpuinfo
 sed -i 's/CPU: ${cpu_usage}/${cpu_usage}/g' package/lean/autocore/files/arm/sbin/usage
 
 ## 设置主机名
