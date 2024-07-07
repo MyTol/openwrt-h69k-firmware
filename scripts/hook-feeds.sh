e6c51088dd5318cf82fcd3290682615b6047bd9a
MyTol <nishiwov1.xiaofang@gmail.com>
2024-07-08 00:19:02 +0800

    修改编译脚本

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -87,6 +87,7 @@ else
   rm -rf package/wwan
   git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/wwan
   rm -rf package/wwan/quectel_Gobinet
+  rm -rf package/wwan/rooter
   git clone --depth=1 https://github.com/my-world-only-me/modem package/wwan/modem
   cp -r package/wwan/modem/driver/quectel_Gobinet package/wwan
   rm -rf package/wwan/modem
