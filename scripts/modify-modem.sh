2d43ab3951a59dffad46f815331baa2a46cc9851
MyTol <nishiwov1.xiaofang@gmail.com>
2024-07-09 00:27:53 +0800

    修改编译脚本

--- scripts/modify-modem.sh
+++ scripts/modify-modem.sh
@@ -16,10 +16,10 @@ if [ "$MODIFY_MODEM" = "true" ]; then
   sed -i 's/wwan_5g_${modem_no}/wwan_${modem_no}/g' package/wwan/app/luci-app-modem/root/usr/share/modem/modem_network_task.sh
   sed -i 's/wwan6_5g_${modem_no}/wwan6_${modem_no}/g' package/wwan/app/luci-app-modem/root/usr/share/modem/modem_network_task.sh
   ## 修复 FM160-CN 模块电话号码获取
-  cp -f $GITHUB_WORKSPACE/data/moify_modem/fibocom.sh package/wwan/app/luci-app-modem/root/usr/share/modem/fibocom.sh
+  cp -f $GITHUB_WORKSPACE/data/modify_modem/fibocom.sh package/wwan/app/luci-app-modem/root/usr/share/modem/fibocom.sh
   ## 替换静态页面
-  cp -f $GITHUB_WORKSPACE/data/moify_modem/modem_info.htm package/wwan/app/luci-app-modem/luasrc/view/modem/modem_info.htm
-  cp -f $GITHUB_WORKSPACE/data/moify_modem/modem.lua package/wwan/app/luci-app-modem/luasrc/controller/modem.lua
+  cp -f $GITHUB_WORKSPACE/data/modify_modem/modem_info.htm package/wwan/app/luci-app-modem/luasrc/view/modem/modem_info.htm
+  cp -f $GITHUB_WORKSPACE/data/modify_modem/modem.lua package/wwan/app/luci-app-modem/luasrc/controller/modem.lua
   ## 调整本地化文本
   sed -i 's/msgstr "网络模式"/msgstr "网络模式 (Network Mode)"/g' package/wwan/app/luci-app-modem/po/zh-cn/modem.po
 else
@@ -29,10 +29,10 @@ else
   sed -i 's/wwan_5g_${modem_no}/wwan_${modem_no}/g' package/wwan/luci-app-modem/root/usr/share/modem/modem_network_task.sh
   sed -i 's/wwan6_5g_${modem_no}/wwan6_${modem_no}/g' package/wwan/luci-app-modem/root/usr/share/modem/modem_network_task.sh
   ## 修复 FM160-CN 模块电话号码获取
-  cp -f $GITHUB_WORKSPACE/data/moify_modem/fibocom.sh package/wwan/luci-app-modem/root/usr/share/modem/fibocom.sh
+  cp -f $GITHUB_WORKSPACE/data/modify_modem/fibocom.sh package/wwan/luci-app-modem/root/usr/share/modem/fibocom.sh
   ## 替换静态页面
-  cp -f $GITHUB_WORKSPACE/data/moify_modem/modem_info.htm package/wwan/luci-app-modem/luasrc/view/modem/modem_info.htm
-  cp -f $GITHUB_WORKSPACE/data/moify_modem/modem.lua package/wwan/luci-app-modem/luasrc/controller/modem.lua
+  cp -f $GITHUB_WORKSPACE/data/modify_modem/modem_info.htm package/wwan/luci-app-modem/luasrc/view/modem/modem_info.htm
+  cp -f $GITHUB_WORKSPACE/data/modify_modem/modem.lua package/wwan/luci-app-modem/luasrc/controller/modem.lua
   ## 调整本地化文本
   sed -i 's/msgstr "网络模式"/msgstr "网络模式 (Network Mode)"/g' package/wwan/luci-app-modem/po/zh-cn/modem.po
 fi
