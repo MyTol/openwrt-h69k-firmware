65a8e9f86d1b9dd53cfa9591b70e48c4bfdfbabc
MyTol <nishiwov1.xiaofang@gmail.com>
2024-07-10 16:14:30 +0800

    修改编译脚本

--- scripts/modify-theme.sh
+++ scripts/modify-theme.sh
@@ -11,7 +11,8 @@
 pushd package/community/luci-theme-argon/htdocs/luci-static/argon/css/
 
 sed -i 's|content: "\e90d";|content: "{0}";|g' fonts.css
-sed -i 's|content: "\e913";|content: "{1}";|g' fonts.css
+sed -i 's|content: "\e915";|content: "{1}";|g' fonts.css
+
 
 sed -i 's|content: "{0}";|content: "\e913";|g' fonts.css
 sed -i 's|content: "{1}";|content: "\e90d";|g' fonts.css
